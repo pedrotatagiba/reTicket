03f86994223958ea21e6a344ca7d558f46efcb88
João Rosauro <rosaurojoao@gmail.com>
2025-06-23 17:27:57 -0300

    admin

--- reTicket/ingressos/admin.py
+++ reTicket/ingressos/admin.py
@@ -3,6 +3,5 @@ from ingressos.models import Ingresso
 
 @admin.register(Ingresso)
 class IngressoAdmin(admin.ModelAdmin):
-    list_display = ('evento', 'codigo', 'dtEvento', 'preco')
-    search_fields = ('evento')
-    list_filter = ('dtEvento')
+    list_filter = ['dtEvento']
+    search_fields = ['evento']
