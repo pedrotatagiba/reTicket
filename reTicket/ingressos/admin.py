5e391e7797cf2fc42837d03d5313fb333670642a
João Rosauro <rosaurojoao@gmail.com>
2025-06-23 17:22:02 -0300

    admin

--- reTicket/ingressos/admin.py
+++ reTicket/ingressos/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Ingresso
+from ingressos.models import Ingresso
 
 @admin.register(Ingresso)
 class IngressoAdmin(admin.ModelAdmin):
     list_display = ('evento', 'codigo', 'dtEvento', 'preco')
-    search_fields = ('evento',)
-    list_filter = ('dtEvento',)
+    search_fields = ('evento')
+    list_filter = ('dtEvento')
