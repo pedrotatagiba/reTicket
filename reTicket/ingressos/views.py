06d0a801a5bf77d86181a337ac7d03d533f2e136
pedrotatagiba <pedrotatagiba.g@gmail.com>
2025-06-27 10:13:29 -0300

    ingressos

--- reTicket/ingressos/views.py
+++ reTicket/ingressos/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from ingressos.models import Ingresso
 from django.contrib.auth.decorators import login_required
-
+from django.views.generic.base import View
 
 def buscaUmIngresso(request):
   return render(request, 'ingressos/buscaUmIngresso.html')
-
 
 def respostaBuscaUmIngresso(request):
   evento = Ingresso.objects.all().filter(
     evento__icontains=request.GET.get('evento'))
   contexto = { 'evento': evento, }
   return render(request, 'ingressos/listaIngressos.html', contexto)
+
+class IngressoListView(View):
+ def get(self, request, *args, **kwargs):
+  ingressos = Ingresso.objects.all()
+  contexto = { 'ingressos': ingressos, }
+  return render(request, 'contatos/listaIngressos.html', contexto)
